b93d6c86f23972d9c564e420f94f58d2ac87e3a8
Panzerschrek <AntiPanzerschrek@yandex.ru>
2015-02-23 15:25:24 +0700

    Добавил архивирование базы данных git

--- 2zip.bat
+++ 2zip.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a Micro-F-full.zip src/* models/* *.sln *.vcproj *.dll Micro-F.exe Micro-F-upx.exe *.bat stub.asm stub.exe
+"C:\Program Files\7-Zip\7z.exe" a Micro-F-full.zip src/* models/* *.sln *.vcproj *.dll Micro-F.exe Micro-F-upx.exe *.bat stub.asm stub.exe .git/* .gitignore README.md
 PAUSE
