b8de13c1174fd1a2dce8738d2f13b2b5208269f8
Panzerschrek <AntiPanzerschrek@yandex.ru>
2015-02-23 16:26:20 +0700

    Добавил забытые файлы

---
+++ models/models.cpp
@@ -0,0 +1,10 @@
+#include "models.h"
+
+namespace mf_Models
+{
+
+const unsigned char f1949[]=
+#include "f1949.h"
+;
+
+} // namespace mf_Models
---
+++ models/models.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace mf_Models
+{
+
+extern const unsigned char f1949[];
+
+} // namespace mf_Models
