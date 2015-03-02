27e353e1ea14aaeaaa7a38ab8ca81e477b692cd9
Panzerschrek <AntiPanzerschrek@yandex.ru>
2015-03-02 21:13:40 +0700

    Zapilil modelj zvezdy (a ne vojny)

--- models/models.cpp
+++ models/models.cpp
@@ -11,4 +11,16 @@ const unsigned char f2xxx[]=
 #include "f2xxx.h"
 ;
 
+const unsigned char cube[]=
+#include "cube.h"
+;
+
+const unsigned char icosahedron[]=
+#include "icosahedron.h"
+;
+
+const unsigned char star[]=
+#include "star.h"
+;
+
 } // namespace mf_Models
--- models/models.h
+++ models/models.h
@@ -7,4 +7,10 @@ extern const unsigned char f1949[];
 
 extern const unsigned char f2xxx[];
 
+extern const unsigned char cube[];
+
+extern const unsigned char icosahderon[];
+
+extern const unsigned char star[];
+
 } // namespace mf_Models
